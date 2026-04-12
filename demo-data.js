174be1427ae073deb0317885d8307def9f67a38f
Nguyen Hoang Phuc Huy <hoangphuchuy.nguyen@zuhlke.com>
2026-04-12 15:14:20 +0700

    Add first commits

---
+++ demo-data.js
@@ -0,0 +1,9 @@
+window.siteData = {
+  backgroundText: 'I LOVE YOU',
+  backgroundColor: '#ffa3e0',
+  textColor: { r: 179, g: 204, b: 255 },
+  heartColor: { r: 255, g: 105, b: 180 },
+  messages: ['Happy Birthday', 'MY ANH NGUYEN', '19+', '28.03.2026'],
+  finalText: 'CHÚC EM TUỔI MỚI HẠNH PHÚC BÊN ANH NHA❤',
+  music: '',
+};
